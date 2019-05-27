aa2b11abc1c72a421f6e9c18ac0a167dba94bb24
Ian <ian.git@shawpad.com>
2019-05-26 21:12:39 -0700

    updated gadget config line and mnt point
    scripts now use /mnt/cam for gadget mount point

--- usb_share.py
+++ usb_share.py
@@ -3,24 +3,30 @@
 # original source from aritcle : https://www.raspberrypi.org/magpi/pi-zero-w-smart-usb-flash-drive/
 # original source : sudo wget http://rpf.io/usbzw -O usb_share.py
 
+# assumes the usb cam filesystem is mounted
+# runs an fsck before mounting the gadget
+# sets up a handler to monitor the for changed on the usb ota filesystem
+# after a duration of x following the the first filesystem update, the filesystem is unmounted / cleaned / 
+#  resynced and remounted
+# that updates the local filesystem cache which will then allow follow on code to check for changes...
+
 # to add: 
-#    monitor for music folder
-#    periodic remount and monitor for no change from writing from Tesla side, then fsck
-#      gotta decide on the ro/rw handler for the TeslaCam folder ... 
-#      i.e. monitor for TeslaCam to stop writing, fsck, change to ro, move saved files off, change back to rw 
+#   only run the unmount sequence if there is wifi present that we can use, otherwise no need to sync cache
+#   monitor for the changes... if changed and wifi present, then continue with the unmount to run the upload cloud function
 
 import time
 import os
 from watchdog.observers import Observer
 from watchdog.events import *
 
-CMD_MOUNT = "modprobe g_mass_storage file=/piTeslaCam.bin,/piTeslaMusic.bin stall=0,0, ro=0,0 removable=1,1"
-CMD_UNMOUNT = "modprobe -r g_mass_storage"
+USB_CAM_SHARE_MOUNT_POINT = "/mnt/cam"
+CMD_MOUNT_GADGET = "modprobe g_mass_storage" # specifics moved into config file during create "file=/piTeslaCam.bin,/piTeslaMusic.bin stall=0,0, ro=0,0 removable=1,1"
+CMD_UNMOUNT_GADGET = "modprobe -r g_mass_storage"
 CMD_SYNC = "sync"
-# make sure fsck the filesystem (tesla bug causing corruption sometimes)
-CMD_FSCK = "fsck -a /mnt/usb_share"
+# make sure fsck the filesystem (although Tesla fixed bug, with power drops a non-clean unmount will often occur)
+CMD_FSCK = "fsck -a $USB_CAM_SHARE_MOUNT_POINT"
 
-WATCH_PATH = "/mnt/usb_share"
+WATCH_PATH = $USB_CAM_SHARE_MOUNT_POINT
 ACT_EVENTS = [DirDeletedEvent, DirMovedEvent, FileDeletedEvent, FileModifiedEvent, FileMovedEvent]
 ACT_TIME_OUT = 30
 
@@ -46,9 +52,9 @@ class DirtyHandler(FileSystemEventHandler):
         self._dirty_time = 0
         self._path = None
 
-# check filesystem before presenting the gadget as currently Tesla s/w corrupts fs periodically on shutdown
+# check filesystem before presenting the gadget
 os.system(CMD_FSCK)
-os.system(CMD_MOUNT)
+os.system(CMD_MOUNT_GADGET)
 
 evh = DirtyHandler()
 observer = Observer()
@@ -61,13 +67,13 @@ try:
             time_out = time.time() - evh.dirty_time
 
             if time_out >= ACT_TIME_OUT:
-                os.system(CMD_UNMOUNT)
+                os.system(CMD_UNMOUNT_GADGET)
                 time.sleep(1)
                 os.system(CMD_SYNC)
                 time.sleep(1)
                 os.system(CMD_FSCK)
                 time.sleep(1)
-                os.system(CMD_MOUNT)
+                os.system(CMD_MOUNT_GADGET)
                 evh.reset()
 
             time.sleep(1)
